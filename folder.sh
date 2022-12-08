c8290e6896f296102b666337a3c35129e35da303
nejcmori29 <120044511+nejcmori29@users.noreply.github.com>
2022-12-08 22:06:52 +0100

    s

--- folder.sh
+++ folder.sh
@@ -19,7 +19,7 @@ sudo apt-get install gnupg -y
 sudo apt-get install lsb-release -y
 sudo mkdir -p /etc/apt/keyrings 
 curl -fsSl https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
-
+s
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
