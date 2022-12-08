9d04292ecd6924d0b529559c6d35d4eb0ffdeea1
nejcmori29 <120044511+nejcmori29@users.noreply.github.com>
2022-12-08 20:10:27 +0100

    Update folder.sh

--- folder.sh
+++ folder.sh
@@ -11,16 +11,3 @@ do
 sudo adduser user&i
 sudo mkdir domov&i
 done
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt-get update -y
-sudo apt-get install ca-certificates curl gnupg lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSl https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
-
-echo \
-"deb [arch=&(dkpg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update -y
-
-sudo apt-get install docker-ce docker-ce-cli container.io docker-compose-plugin -y
