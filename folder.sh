87fe4d3ff327e4dcc14016342bf9c8ec37ba7f0d
nejcmori29 <120044511+nejcmori29@users.noreply.github.com>
2022-12-08 21:13:55 +0100

    Update folder.sh

--- folder.sh
+++ folder.sh
@@ -11,6 +11,7 @@ do
 sudo useradd -m --groups sudo uporabnik$m
 done
 dpkg --configure --a
+sudo dpkg -i initrd.img-5.15.0-52-generic
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install ca-certificates -y
