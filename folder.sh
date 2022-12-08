86196f838e64f4592fad389d3d651b26c881c014
nejcmori29 <120044511+nejcmori29@users.noreply.github.com>
2022-12-08 19:53:39 +0100

    Update folder.sh

--- folder.sh
+++ folder.sh
@@ -11,9 +11,9 @@ do
 sudo adduser user&i
 sudo mkdir domov&i
 done
-sudo apt update
-sudo apt upgrade
-sudo apt-get update
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get update -y
 sudo apt-get install \
 ca-certificates\
 curl\
@@ -25,6 +25,6 @@ curl -fsSl https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
 "deb [arch=&(dkpg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
+sudo apt-get update -y
 
 sudo apt-get install docker-ce docker-ce-cli container.io docker-compose-plugin
