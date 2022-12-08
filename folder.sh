0cba34b61592f7f3c6b0552e05560a039ea57816
nejcmori29 <120044511+nejcmori29@users.noreply.github.com>
2022-12-08 22:04:47 +0100

    ss

--- folder.sh
+++ folder.sh
@@ -19,6 +19,9 @@ sudo apt-get install curl -y
 sudo apt-get install gnupg -y 
 sudo apt-get install lsb-release -y
 sudo mkdir -p /etc/apt/keyrings 
-curl -fsSl https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg 
+curl -fsSl https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
+sudo snap install docker
 sudo apt-get install docker-ce docker-ce-cli container.io docker-compose-plugin -y
